2833f95052f049fa19125455994eb796578a414e
Petter Edvardsen <pedvardt@gmail.com>
2023-10-22 22:29:35 +0200

    Main menu and pause screen interface comlpleted. Added Post-Processing.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
     public SceneManager sceneManager;
+    public GameObject ui;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    private bool isPaused = false;
 
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseGame();
+        }
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            ui.SetActive(true);
+
+            // Turns off post-processing
+            Camera.main.gameObject.GetComponent<PostProcessVolume>().enabled = true;
+
+
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            ui.SetActive(false);
+
+            // Turns on post-processing
+            Camera.main.gameObject.GetComponent<PostProcessVolume>().enabled = false;
+        }
+    }
+
+    public void TogglePauseGame()
+    {
+        isPaused = !isPaused;
     }
 
     public void ChangeScene()

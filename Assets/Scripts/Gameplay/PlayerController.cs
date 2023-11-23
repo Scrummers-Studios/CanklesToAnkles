4ef657aeffe851d439c51d4b18192b47ea58caa9
Edvardsn <petter-edvardsen@hotmail.com>
2023-11-23 20:56:07 +0100

    Bugfix: Bouncing player. Refactoring of Playercontroller.

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
@@ -11,11 +12,10 @@ public class PlayerController : MonoBehaviour
     private float BASE_PLAYER_DEPTH;
 
     // Movement properties
-    public float jumpingPower = 35;
+    public float jumpingPower = 100f;
     public bool isGrounded = true;
     public bool isCrouched = false;
     public bool isJumping = false;
-    private float lastJumpTime;
 
     // Conditions for player crouching, Assumes player starts in a standing posistion
     private bool correctedPlayerCrouchOffset = false;
@@ -40,9 +40,6 @@ public class PlayerController : MonoBehaviour
         BASE_PLAYER_WIDTH = playerCollider.size.x;
         BASE_PLAYER_HEIGHT = playerCollider.size.y;
         BASE_PLAYER_DEPTH = playerCollider.size.z;
-
-
-        lastJumpTime = Time.time;
     }
 
     // Used for input related logic
@@ -127,23 +124,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Checks if the player has vertical velocity
-    private bool HasVerticalVelocity()
-    {
-        if (playerRigidBody.velocity.y == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
     // Updates the status of the player being grounded
     private void UpdateGroundedStatus()
     {
-        float rayLength = .01f;
+        float rayLength = .1f;
 
         // Checks if the player is grounded
         isGrounded = Physics.Raycast(playerCollider.bounds.center, UnityEngine.Vector3.down, playerCollider.bounds.extents.y + rayLength);
@@ -154,14 +138,16 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void OnPlayerJumpEvent()
     {
-
-        // Temporary solution for bouncing
-        // Physics: Jump equation, sqrt(2*g*h)
         if (isGrounded && isJumping)
         {
-            playerRigidBody.velocity.Set(0, 0, 0);
+            float velocityReset = -(playerRigidBody.velocity.y * playerRigidBody.mass);
 
-            Debug.Log("Ground velocity upon jump: " + playerRigidBody.velocity.y);
+            Debug.Log("Required force to reset velocity" + velocityReset);
+
+            // Resets the player momentum 
+            playerRigidBody.AddForce(new UnityEngine.Vector3(0f, velocityReset), ForceMode.Impulse);
+
+            // Applies the jumping force
             playerRigidBody.AddForce(new UnityEngine.Vector3(0f, jumpingPower), ForceMode.Impulse);
 
             animator.SetBool("isJumping", true);
